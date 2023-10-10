fed6b5d4ad99473f585b6f4d7f1f0f212f555017
Genesis <loda.tagulao.up@phinmaed.com>
2023-10-10 21:24:33 +0800

    added person outline

---
+++ app/src/main/java/com/example/upangeats/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.upangeats
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.upangeats.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/upangeats/adapter/OrderTrayRecyclerViewAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.upangeats.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class OrderTrayRecyclerViewAdapter : RecyclerView.Adapter<OrderTrayViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderTrayViewHolder {
+        TODO("Not yet implemented")
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: OrderTrayViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class OrderTrayViewHolder(private val view: View):RecyclerView.ViewHolder(view){
+
+}
