0d1270d04690f4e832fe41de4b42423f7a2c11a9
CJ Soriano <soriano.christianjose.m@gmail.com>
2023-10-13 02:00:11 +0800

    Added a child fragment to the fragment_home.xml. Edited UI/UX for the bottom navigation

--- app/src/main/java/com/example/upangeats/HomeHomeFragment.kt
+++ app/src/main/java/com/example/upangeats/HomeHomeFragment.kt
@@ -1,15 +1,22 @@
 package com.example.upangeats
 
-import android.annotation.SuppressLint
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.upangeats.databinding.FragmentHomeHomeBinding
 
-class HomeHomeFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_homehome)
+class HomeHomeFragment : Fragment() {
+    private lateinit var binding : FragmentHomeHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeHomeBinding.inflate(inflater, container, false)
 
+
+        return binding.root
     }
 }
