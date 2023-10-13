a456e2e485725ae524f7bc76cf833a1606735240
neil <nede.deguzman.up@phinmaed.com>
2023-10-13 20:28:43 +0800

    Stalls UI

--- app/src/main/java/com/example/upangeats/StallsFragment.kt
+++ app/src/main/java/com/example/upangeats/StallsFragment.kt
@@ -6,12 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.upangeats.adapter.stallsAdapter
 import com.example.upangeats.databinding.FragmentStallsBinding
 
 class StallsFragment : Fragment() {
     private lateinit var binding : FragmentStallsBinding
+    val stallList = listOf<Stalls>(
+        Stalls( "Aljon","Aljon"),
+        Stalls("Jose","Jose"),
+        Stalls("Menard","Menard"),
+        Stalls("Lord", "Lord"),
+        Stalls("Jeremy", "Jeremy"),
+        Stalls("Neil", "Neil"),
+        Stalls("Julius", "Julius")
+    )
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -19,12 +31,24 @@ class StallsFragment : Fragment() {
         val binding = FragmentStallsBinding.inflate(inflater, container, false)
         binding.stallsrecyclerView.layoutManager = LinearLayoutManager(this.context)
         binding.stallsrecyclerView.setBackgroundColor(Color.GREEN)
-        binding.stallsrecyclerView.adapter = stallsAdapter()
+        binding.stallsrecyclerView.adapter = stallsAdapter(
+            stallList,
+        ) { selectedItem: Stalls ->
+            listItemClicked(selectedItem)
+        }
 
 
 
 
 
         return binding.root
+    }
+
+    private fun listItemClicked(stalls: Stalls){
+        Toast.makeText(
+            this.context,
+            "Stall of ${stalls.supplier}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
